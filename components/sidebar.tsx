65a35702ee11c91eeb8b316df9e097cb55ea39ef
Dixit Lukhi-LD <lukhidixit149@gmail.com>
2023-10-25 16:35:47 +0530

    redirect to home

--- components/sidebar.tsx
+++ components/sidebar.tsx
@@ -27,7 +27,7 @@ const Sidebar: React.FC = () => {
   return (
     <div className="space-y-4 py-4 flex flex-col h-full bg-[#111827] text-white">
       <div className="px-3 py-2 flex-1">
-        <Link href="/dashboard" className="flex items-center pl-3 mb-14">
+        <Link href="/" className="flex items-center pl-3 mb-14">
           <div className="relative w-8 h-8 mr-5">
             <Image src="/logo.png" alt="Logo" fill />
           </div>
